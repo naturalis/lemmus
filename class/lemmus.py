3a4d2a4e83faf6bae290c7c4ab2d0daf62cf85a8
Atze de Vries <atze.devries@naturalis.nl>
2013-08-22 13:05:50 +0200

    written class definitions and initial push

---
+++ class/lemmus.py
@@ -0,0 +1,23 @@
+# Lemmus defines 4 classes
+#
+# Init
+# Init class intializes the lemmus user.
+# - registers github username password
+# - gets te relevant dir's and repo's for developing
+# - potentionally saves userdata locally (~/.lemmus) to ensure continuation after disaster
+#
+# Issue
+# Issue class manages software issues and provides a workflow for fixing, testing etc by using github issue tracking
+# - list current issues with creator and assignee and comments (or link to comments)
+# - add an issue
+# - close an issues
+# - test a fix
+# - delete an issue
+#
+# Review
+# Review class provides tools for reviewing written code
+# - does stuff
+#
+# ArgumentParse
+# ArgumentPare class provides a parser for lemmus arguments and switches. Also provides the help
+# - does stuff
